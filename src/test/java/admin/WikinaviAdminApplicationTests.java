41431a49f283e2a889e63a378ce3a74c5b50cc38
miki1029 <miki1029g@gmail.com>
2015-10-04 16:02:03 +0900

    init spring boot

---
+++ src/test/java/admin/WikinaviAdminApplicationTests.java
@@ -0,0 +1,18 @@
+package admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WikinaviAdminApplication.class)
+@WebAppConfiguration
+public class WikinaviAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

6ec30e4363017d8c66d337fe6aedaf7a4ab3cb32
miki1029 <miki1029g@gmail.com>
2015-10-04 17:15:11 +0900

    apply spring-mvc

--- src/test/java/khc/wikinavi/admin/WikinaviAdminApplicationTests.java
+++ src/test/java/khc/wikinavi/admin/WikinaviAdminApplicationTests.java
@@ -1,4 +1,4 @@
-package admin;
+package khc.wikinavi.admin;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = WikinaviAdminApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class WikinaviAdminApplicationTests {
 
